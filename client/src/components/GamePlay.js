fdf7a6fceaffcb129d31b6a52b17e9ababb07c12
“Laura <laurakekesi@gmail.com>
2022-08-09 16:39:30 -0400

    Starting game functionality

--- client/src/components/GamePlay.js
+++ client/src/components/GamePlay.js
@@ -3,173 +3,174 @@ import styled from "styled-components";
 import TimerBar from "./TimerBar";
 
 const GamePlay = () => {
+  const [triviaQuestions, setTriviaQuestions] = useState(null);
+  const [triviaIndex, setTriviaIndex] = useState(null);
+  const [pointTally, setPointTally] = useState(0);
+  const [gameState, setGameState] = useState("pause");
 
-const [triviaQuestions, setTriviaQuestions] = useState(null);
+  useEffect(() => {
+    fetch("/api/triviaQuestions")
+      .then((res) => res.json())
+      .then((data) => {
+        setTriviaQuestions(data.data);
+        setTriviaIndex(0);
+        console.log("Use effect", data.data);
+      })
+      .catch((err) => console.log("err", err));
+  }, []);
 
-    useEffect(() => {
-        fetch('/api/triviaQuestions')
-        .then((res) => res.json())
-        .then((data) => {
-            setTriviaQuestions(data.data);
-        })
-        .catch((err) => console.log("err", err))
-    }, [])
 
+  useEffect(() => {
+    setGameState("play");
+    setTimeout(() => {
+      setGameState("result");
+      setTimeout(() => {
+          if (triviaIndex < 19) {
+          setTriviaIndex(triviaIndex+1)
+        } else {
+            setGameState("gameOver");
+        }
+      }, 5000)
+    }, 15000);
+    
+  }, [triviaIndex]);
 
+  
+  //if state ==== play, settimeout 15s
 
-const notFetch =  {
-    "category": "General Knowledge",
-    "id": "622a1c357cc59eab6f94fc94",
-    "correctAnswer": "Blatherskite",
-    "incorrectAnswers": [
-      "Agastopia",
-      "Cacophony",
-      "Impignorate"
-    ],
-    "question": "Which word is defined as 'a person who talks at great length without making much sense'?",
-    "tags": [
-      "words",
-      "general_knowledge"
-    ],
-    "type": "Multiple Choice",
-    "difficulty": "hard",
-    "regions": []
-  }
+  if (triviaQuestions && gameState === "play") {
+    // console.log(triviaQuestions[0]);
+    let currentQuestion = triviaQuestions[triviaIndex];
 
-//map over the whole notFetched quesstion array, do this for each item. 
-
-  //creates an array consisting of the 3 incorrect answers and the 1 correct answer.
-const createAnswersArray = (object) => {
-    let answersArray = [];
-    answersArray.push(notFetch.correctAnswer);
-    notFetch.incorrectAnswers.map((answer) => answersArray.push(answer))
-    // return answersArray;
-    const shuffled = answersArray.sort(()=>{
+    const createAnswersArray = (object) => {
+      let answersArray = [];
+      answersArray.push(currentQuestion.correctAnswer);
+      currentQuestion.incorrectAnswers.forEach((answer) =>
+        answersArray.push(answer)
+      );
+      //shuffles the answers array
+      const shuffled = answersArray.sort(() => {
         return Math.random() - 0.5;
-    })
-    return shuffled;
-}
+      });
+      return shuffled;
+    };
 
-const shuffledAnswers = createAnswersArray();
-const correctAnswer = notFetch.correctAnswer;
-//set timeout, after 15 seconds, if no answer is selected, points = 0
-//else
-//create a function where on click, 
-// 1) if correct answer, give points, else 0 points
-// push points to points tally (array?)
-// change border colours to red or green
-//either way, after 15 seconds, move on to next question.
+    const shuffledAnswers = createAnswersArray();
+    const correctAnswer = currentQuestion.correctAnswer;
+    //set timeout, after 15 seconds, if no answer is selected, points = 0
+    //else
+    //create a function where on click,
+    // 1) if correct answer, give points, else 0 points
+    // push points to points tally (array?)
+    // change border colours to red or green
+    //either way, after 15 seconds, move on to next question.
 
-    return(
-        <Wrapper>
-            <BackgroundDiv>
-                <GameDiv>
-                    <TimerDiv>
-                        <TimerBar/>
-                    </TimerDiv>
-                    <QuestionDiv>
-                        <QuestionIndex>1/20</QuestionIndex>
-                        <div>{notFetch.question}</div>
-                    </QuestionDiv>
-                    <AnswersDiv>
-                        {shuffledAnswers.map((answer)=>{
-                            return(
-                                (answer===correctAnswer)? 
-                                <CorrectAnswer value={answer}>
-                                    {answer}
-                                </CorrectAnswer>
-                            :
-                                <IncorrectAnswer value={answer}>
-                                    {answer}
-                                </IncorrectAnswer>
-                            )
-                        })}
-                    </AnswersDiv>
-                </GameDiv>
-            </BackgroundDiv>
-        </Wrapper>
-    )
-}
+    return (
+      <Wrapper>
+        <BackgroundDiv>
+          <GameDiv>
+            <TimerDiv>
+              <TimerBar />
+            </TimerDiv>
+            <QuestionDiv>
+              <QuestionIndex>1/20</QuestionIndex>
+              <div>{currentQuestion.question}</div>
+            </QuestionDiv>
+            <AnswersDiv>
+              {shuffledAnswers.map((answer) => {
+                return answer === correctAnswer ? (
+                  <CorrectAnswer value={answer}>{answer}</CorrectAnswer>
+                ) : (
+                  <IncorrectAnswer value={answer}>{answer}</IncorrectAnswer>
+                );
+              })}
+            </AnswersDiv>
+          </GameDiv>
+        </BackgroundDiv>
+      </Wrapper>
+    );
+  }
+};
 const QuestionIndex = styled.div`
-font-family: var(--header-font-family);
-font-size: 30px;
-margin-bottom: 10px;
-`
+  font-family: var(--header-font-family);
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
 
 const TimerDiv = styled.div`
-height: 15%;
-display: flex; 
-align-items: center;
-`
+  height: 15%;
+  display: flex;
+  align-items: center;
+`;
 const IncorrectAnswer = styled.button`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 40%;
-width: 45%;
-border-radius: 10px;
-background: rgba(255,255,255,0.5);
-border: var(--orangey-yellow) 4px solid;
-font-family: var(--secondary-font-family);
-font-size: 25px;
-cursor: pointer;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40%;
+  width: 45%;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.5);
+  border: var(--orangey-yellow) 4px solid;
+  font-family: var(--secondary-font-family);
+  font-size: 25px;
+  cursor: pointer;
+`;
 const CorrectAnswer = styled.button`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 40%;
-width: 45%;
-border-radius: 10px;
-/* background: rgba(255,255,255,0.5); */
-background: pink;
-border: var(--orangey-yellow) 4px solid;
-cursor: pointer;
-font-family: var(--secondary-font-family);
-font-size: 25px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40%;
+  width: 45%;
+  border-radius: 10px;
+  /* background: rgba(255,255,255,0.5); */
+  background: pink;
+  border: var(--orangey-yellow) 4px solid;
+  cursor: pointer;
+  font-family: var(--secondary-font-family);
+  font-size: 25px;
+`;
 const QuestionDiv = styled.div`
-height: 30%;
-border-radius: 10px;
-background: rgba(255,255,255,0.5);
-border: var(--orangey-yellow) 4px solid;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-font-size: 25px;
-`
+  height: 30%;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.5);
+  border: var(--orangey-yellow) 4px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+`;
 const AnswersDiv = styled.div`
-height: 50%;
-column-count: 2;
-display: flex;
-flex-direction: column;
-flex-wrap: wrap;
-justify-content: center;
-align-items: center;
-gap: 15px;
-`
+  height: 50%;
+  column-count: 2;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
 const GameDiv = styled.div`
-width: 85%;
-height: 90%;
-font-family: var(--secondary-font-family);
-`
+  width: 85%;
+  height: 90%;
+  font-family: var(--secondary-font-family);
+`;
 const BackgroundDiv = styled.div`
-width: 85%;
-height: 85%;
-background: rgba(172,216,210, 0.5);
-border-radius: 15px;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  width: 85%;
+  height: 85%;
+  background: rgba(172, 216, 210, 0.5);
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Wrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
 
-/* added in because of navbar stuff */
-padding-top: 70px;
-`
+  /* added in because of navbar stuff */
+  padding-top: 70px;
+`;
 export default GamePlay;
