940e206d37f422a0d4e04dfcd94a0a827e82d7cf
“Laura <laurakekesi@gmail.com>
2022-08-09 11:51:20 -0400

    Created profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,27 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
 const Profile = () => {
+    const userId = useParams();
+    console.log(userId);
+    //use params to get id
+    //create top div with profile pic, name, high score, top subject
+
+    //create bottom div with all posts by person
+    //map over all posts by person, render userPost with {postid = not sure yet} as prop
     return(
-        <>
-        </>
+        <Wrapper>
+            <PersonInfo></PersonInfo>
+            <PostsByPerson></PostsByPerson>
+        </Wrapper>
     )
 }
+
+const PersonInfo = styled.div`
+`
+const PostsByPerson = styled.div`
+`
+const Wrapper = styled.div`
+`
 
 export default Profile;
